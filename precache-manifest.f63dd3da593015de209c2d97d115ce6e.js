fb31799acc519b404c5d2cc5038dd658ff3ae54e
TanyaD <tat.dbk@gmail.com>
2019-08-02 13:27:55 -0700

    Updates

---
+++ precache-manifest.f63dd3da593015de209c2d97d115ce6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54242110b364e34fcd1c8debe04d6a19",
+    "url": "/react-template/index.html"
+  },
+  {
+    "revision": "83c1d14e3c2854cf4bcf",
+    "url": "/react-template/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1d1aab485550f1e682ca",
+    "url": "/react-template/static/css/main.cdb4415d.chunk.css"
+  },
+  {
+    "revision": "83c1d14e3c2854cf4bcf",
+    "url": "/react-template/static/js/2.10b73b5c.chunk.js"
+  },
+  {
+    "revision": "1d1aab485550f1e682ca",
+    "url": "/react-template/static/js/main.c70102c6.chunk.js"
+  },
+  {
+    "revision": "d5e8df4740b7c9a27459",
+    "url": "/react-template/static/js/runtime~main.0455cb28.js"
+  }
+]);
