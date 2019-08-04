0885658dbc85bf9303f42e484d7d317e132a4986
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-08-03 23:44:20 -0700

    Add sort of appointments

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,8 @@ class App extends Component {
     this.state = {
       myAppointments: [],
       formDisplay: false,
+      orderBy: 'petName',
+      orderDir: 'asc',
       lastIndex:0
     }
     this.deleteAppointment = this.deleteAppointment.bind(this);
@@ -65,6 +67,26 @@ class App extends Component {
   }
 
   render(){
+    let order;
+    let filteredApts = this.state.myAppointments;
+    if(this.state.orderDir === 'asc'){
+      order = 1;
+    }
+    else{
+      order = -1;
+    }
+    
+    filteredApts.sort((a,b) => {
+      if(a[this.state.orderBy].toLowerCase()<
+        b[this.state.orderBy].toLowercase()
+        ){
+        return -1 * order;
+      }
+      else{
+        return 1* order;
+      }
+  });
+
   return (
     <main className="page bg-white" id="petraitings">
       <div className="container">
@@ -77,7 +99,7 @@ class App extends Component {
                 addAppointment={this.addAppointment}
               />
               <SearchAppointments />
-              <ListAppointments appointments={this.state.myAppointments} deleteAppointment={this.deleteAppointment}/>
+              <ListAppointments appointments={filteredApts} deleteAppointment={this.deleteAppointment}/>
             </div>
           </div>
         </div>
