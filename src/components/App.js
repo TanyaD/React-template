e92b9e5aba9aca78c8ea03c1bdf7a03d105d73f5
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-08-07 16:14:16 -0700

    Add Update functionality

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import ListAppointments from './ListAppointments';
 import SearchAppointments from './SearchAppointments';
 
 import {without} from 'lodash';
+import {findIndex} from 'lodash';
 
 
 class App extends Component {
@@ -25,6 +26,7 @@ class App extends Component {
     this.addAppointment = this.addAppointment.bind(this);
     this.changeOrder = this.changeOrder.bind(this);
     this.searchApts = this.searchApts.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
   }
 
   changeOrder(order,dir){
@@ -37,6 +39,17 @@ class App extends Component {
   searchApts(query){
     this.setState({
       queryText: query
+    })
+  }
+  
+  updateInfo(name,value,id){
+    let tempApts = this.state.myAppointments;
+    let aptIndex = findIndex(this.state.myAppointments, {
+      aptId:id
+    })
+    tempApts[aptIndex][name] = value;
+    this.setState({
+      myAppointments: tempApts
     })
   }
 
@@ -133,6 +146,7 @@ class App extends Component {
               <ListAppointments 
               appointments={filteredApts}
               deleteAppointment={this.deleteAppointment}
+              updateInfo = {this.updateInfo}
               />
             </div>
           </div>
